40decfbcba00dd0baf2817df57e948477a4b6bd4
andreianet <andreia.netsys@hotmail.com>
2021-03-26 10:53:00 -0300

    aula02

---
+++ server/src/constants.js
@@ -0,0 +1,8 @@
+export const constants = {
+    events:{
+        MESSAGE: 'message',
+        NEW_USER_CONNECTED: 'newUserConnected',
+        DISCONNECTED: 'disconnectUser',
+        UPDATE_USERS: 'updateUsers'
+    }
+}
